f04d7409c7332b9bb104330f9b1182ad41b9873e
Elena Alexe <aelexa16@gmail.com>
2021-10-26 22:37:50 +0300

    modif

--- src/ArrayLand.js
+++ src/ArrayLand.js
@@ -29,60 +29,80 @@ class ArrayLand extends React.Component {
       boxes: fruits,
     })
     this.shuffleFruits();
-  }
+  };
 
-  const orderedFruits = fruits.flat().reduce(assignFruit, newBoxes);
+
+  sortBoxes = () => {
+    let newBoxes = {
+      oranges: [],
+      bananas: [],
+      lemons: [],
+    }
+    let fruits = this.state.boxes;
+
+    const assignFruit = (prevValue, currentVal) => {
+      if (currentVal === "🍊") {
+        prevValue.oranges.push(currentVal);
+      } else if (currentVal === "🍌") {
+        prevValue.bananas.push(currentVal);
+      } else if (currentVal === "🍋") {
+        prevValue.lemons.push(currentVal);
+      }
+      return prevValue;
+    }
+
+    const orderedFruits = fruits.flat().reduce(assignFruit, newBoxes);
     this.setState({
-    boxes: [orderedFruits.oranges, orderedFruits.bananas, orderedFruits.lemons],
-  })
-this.shuffleFruits();
+      boxes: [orderedFruits.oranges, orderedFruits.bananas, orderedFruits.lemons],
+    })
+    this.shuffleFruits();
   }
 
-shuffleFruits = () => {
-  if (this.timeout) {
-    clearTimeout(this.timeout);
+  shuffleFruits = () => {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+
+    let fruits = this.state.boxes;
+    const newBoxes = [
+      [],
+      [],
+      [],
+    ]
+
+    const assignFruit = (prevValue, currentVal) => {
+      const randomNumber = Math.floor(Math.random() * 3);
+      newBoxes[randomNumber].push(currentVal);
+      return prevValue;
+    }
+
+    const randomized = fruits.flat().reduce(assignFruit, newBoxes);
+
+    this.timeout = setTimeout(() => {
+      this.setState({
+        boxes: randomized,
+      });
+    }, 3000);
   }
 
-  let fruits = this.state.boxes;
-  const newBoxes = [
-    [],
-    [],
-    [],
-  ]
-
-  const assignFruit = (prevValue, currentVal) => {
-    const randomNumber = Math.floor(Math.random() * 3);
-    newBoxes[randomNumber].push(currentVal);
-    return prevValue;
+  componentWillUnmount() {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
   }
 
-  const randomized = fruits.flat().reduce(assignFruit, newBoxes);
-
-  this.timeout = setTimeout(() => {
-    this.setState({
-      boxes: randomized,
-    });
-  }, 3000);
-}
-
-componentWillUnmount() {
-  if (this.timeout) {
-    clearTimeout(this.timeout);
+  render() {
+    return (
+      <>
+        <h1>Array Land</h1>
+        <br />
+        {this.state.boxes.map((boxes) =>
+          <div className="box">{boxes}</div>)}
+        <button className="sort-fruits" onClick={this.sortFruits}>Click to sort those fruits</button>
+        <button className="sort-fruits" onClick={this.sortBoxes}>Click to sort those boxes</button>
+      </>
+    )
   }
-}
-
-render() {
-  return (
-    <>
-      <h1>Array Land</h1>
-      <br />
-      {this.state.boxes.map((boxes) =>
-        <div className="box">{boxes}</div>)}
-      <button className="sort-fruits" onClick={this.sortFruits}>Click to sort those fruits</button>
-      <button className="sort-fruits" onClick={this.sortBoxes}>Click to sort those boxes</button>
-    </>
-  )
-}
 }
 
 export default ArrayLand;
